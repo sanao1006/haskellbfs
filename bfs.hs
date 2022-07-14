4229ca238b3c1b997ccf39f67e7f784721a710f6
sanao <62137820+sanao1006@users.noreply.github.com>
2022-07-14 09:11:37 +0900

    Update bfs.hs

--- bfs.hs
+++ bfs.hs
@@ -2,21 +2,24 @@ import qualified Data.Sequence as Seq
 import qualified Data.Array.MArray as MA
 import Data.Array.ST
 import Data.STRef
-import Data.Traversable(traverse_)
+import Data.Foldable(traverse_)
+import Data.Array.Unboxed(UArray)
+import qualified Data.Array.IArray as A
+import Control.Monad.ST
+import Control.Monad(when)
 
 type Graph = A.Array Int [Int]
-type Queue = STRef s(Seq.Seq Int)
-
+type Queue s = STRef s(Seq.Seq Int)
 
 bfs :: Int -> Int -> Graph -> UArray Int Int
 bfs start n graph = runSTUArray $ do
   dist <- MA.newArray (0, n-1) (-1) :: ST s(STUArray s Int Int)
-  queue <- newSTRef (Seq.singleton start) :: ST s Queue
+  queue <- newSTRef (Seq.singleton start) :: ST s(Queue s)
   MA.writeArray dist start 0
   bfsInternal dist queue graph
   pure dist
 
-bfsInternal :: STUArray s Int Int -> Queue -> Graph -> ST s()
+bfsInternal :: STUArray s Int Int -> Queue s -> Graph -> ST s()
 bfsInternal dist queue graph = do
   que <- readSTRef queue
   if(Seq.null que) then pure()
